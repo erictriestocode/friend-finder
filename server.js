b6e955f964d82a3f5112477bbfd5af391b1e01fd
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-03-11 21:22:54 -0500

    Completed main Server coding

--- server.js
+++ server.js
@@ -6,4 +6,18 @@ const path = require("path");
 // IMPORTS
 const htmlRoutes = require("./app/routing/htmlRoutes");
 const apiRouts = require("./app/routing/apiRoutes");
+// PORT
+var PORT = process.env.PORT || 8080;
 
+// Data Parsing for Express(exercise 13.3 16 - hot restaurant)
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//ROUTES
+require("./app/routing/apiRoutes");
+require("./app/routing/htmlRoutes");
+
+//Listener
+app.listen(PORT, function(){
+    console.log("Server is listening on: " + PORT);
+});
