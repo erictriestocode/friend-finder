b7a8289be34ee261999db937b4d393f33abcf92c
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-03-11 21:32:05 -0500

    routes are aggravating

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,6 @@
 // API Routes
 const userData = require("../data/friends");
+const path = require("path");
 
 // EXPORTED API ROUTES
 module.exports = function(app){
@@ -7,8 +8,8 @@ module.exports = function(app){
     app.get("/api/friends", function(req,res){
         res.json(userData)
     });
-    app.post("/api/friends", function(req,res){
+    // app.post("/api/friends", function(req,res){
 
-    });
+    // });
 
 }
