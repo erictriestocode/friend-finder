a7edb888ad7e287806fde1348456571c10dc8654
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-03-11 21:23:28 -0500

    Minor Formatting for my liking

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // Server File
 //**************************
+
 // NODE MODULES
 const app = require("express");
 const path = require("path");
+
 // IMPORTS
 const htmlRoutes = require("./app/routing/htmlRoutes");
 const apiRouts = require("./app/routing/apiRoutes");
+
 // PORT
 var PORT = process.env.PORT || 8080;
 
