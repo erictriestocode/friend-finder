4d72d18fdf65fa5d4eb3da48024d8ade88b7ce5b
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-03-11 21:44:40 -0500

    test data added

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+// User Data Storage
+
+let userArray = [
+    {
+        name: "Eric"
+        photo: "http://deprecatedhope.com/assets/images/face@0,1x.png"
+        scores: [
+            5,
+            1,
+            3,
+            5,
+            2,
+            2,
+            5,
+            5,
+            3,
+            4
+        ]
+    }
+]
