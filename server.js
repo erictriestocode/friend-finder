f635d1c6e2a547897dfe7177db7315ac7fec3185
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-03-11 20:51:13 -0500

    importing routes JS files for use

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 // Server File
-
-var app = require("express");
-var PATH = require("path");
+//**************************
+// NODE MODULES
+const app = require("express");
+const path = require("path");
+// IMPORTS
+const htmlRoutes = require("./app/routing/htmlRoutes");
+const apiRouts = require("./app/routing/apiRoutes");
 
